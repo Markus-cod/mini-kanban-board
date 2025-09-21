7db1029e3408d0024ca6abc3ee876aaaebd7b8d4
Markus-cod <mzidar11@gmail.com>
2025-09-22 00:33:54 +0200

    feat(project): refactor Project component to utilize Task component and simplify layout

--- client/src/components/Project.tsx
+++ client/src/components/Project.tsx
@@ -1,5 +1,4 @@
 import {
-    PlusIcon,
     ListIcon,
     GridFourIcon,
     ColumnsIcon,
@@ -7,8 +6,7 @@ import {
     SlidersIcon,
     SortDescendingIcon,
     ExportIcon,
-    ChatTeardropDotsIcon,
-    CheckCircleIcon
+    PlusIcon
 } from '@phosphor-icons/react'
 import PlaceholderLogo from '/PlaceholderLogo.svg'
 
@@ -19,29 +17,14 @@ import {
     Tabs,
     Text,
     Status,
-    Badge,
-    Progress,
-    Avatar,
-    AvatarGroup
+    SimpleGrid
 } from '@chakra-ui/react'
 import { useState } from 'react'
 
+import Task from './Task'
+
 function Project() {
 
-    const users = [
-        {
-            src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/131317.webp?s=d4b03c7291407bde303bc0758047f6bd",
-            name: "Uchiha Sasuke",
-        },
-        {
-            src: "https://cdn.myanimelist.net/r/84x124/images/characters/7/284129.webp?s=a8998bf668767de58b33740886ca571c",
-            name: "Baki Ani",
-        },
-        {
-            src: "https://cdn.myanimelist.net/r/84x124/images/characters/9/105421.webp?s=269ff1b2bb9abe3ac1bc443d3a76e863",
-            name: "Uchiha Chan",
-        },
-    ]
 
     const [value, setValue] = useState<string | null>("grid")
 
@@ -90,8 +73,8 @@ function Project() {
                 </Flex>
                 <Flex>
                     <Tabs.Content value="grid" p={8}>
-                        <Flex gap={6} >
-                            <Flex w={"full"} p={4} bg={"#F8FAFC"} borderRadius={"4xl"} borderColor={"#E2E8F0"} borderWidth={"1px"} direction={"column"} gap={4}>
+                        <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} gap={6}>
+                            <Flex alignSelf={"flex-start"} w={"full"} p={4} bg={"#F8FAFC"} borderRadius={"4xl"} borderColor={"#E2E8F0"} borderWidth={"1px"} direction={"column"} gap={4}>
                                 <Flex gap={4}>
                                     <Flex w={"full"} align={"center"} gap={2}>
                                         <Status.Root>
@@ -104,57 +87,47 @@ function Project() {
                                         <PlusIcon />
                                     </Button>
                                 </Flex>
-                                <Flex direction={"column"} gap={3}>
-                                    <Flex w={"full"} p={3} bg={"white"} borderRadius={"2xl"} borderColor={"#E2E8F0"} borderWidth={"1px"} direction={"column"} gap={4} boxShadow={"md"}>
-                                        <Flex gap={3} direction={"column"} align={"flex-start"}>
-                                            <Badge borderRadius={"full"} bg={"#EEF2FF"} color={"#4F46E5"}>Important</Badge>
-                                            <Text textStyle={"lg"} fontWeight={"bold"} color={"#1E293B"}>UI/UX Design in the age of AI</Text>
-                                        </Flex>
-                                        <Progress.Root
-                                            value={100}
-                                            colorPalette='purple'
-                                            size={"sm"}>
-                                            <Flex w={"full"} gap={2} direction={"column"}>
-                                                <Flex justify={"space-between"} align={"center"} w={"full"}>
-                                                    <Progress.Label color={"#475569"} fontWeight={"medium"} textStyle={"sm"}>
-                                                        Progress
-                                                    </Progress.Label>
-                                                    <Progress.ValueText color={"#475569"} fontWeight={"medium"} textStyle={"sm"} />
-                                                </Flex>
-                                                <Progress.Track borderRadius={"full"}>
-                                                    <Progress.Range borderRadius={"full"} />
-                                                </Progress.Track>
-                                            </Flex>
-                                        </Progress.Root>
-                                        <Flex w={"full"} justify={"space-between"} align={"center"}>
-                                            <AvatarGroup size="lg" stacking="last-on-top">
-                                                {users.map((user) => (
-                                                    <Avatar.Root key={user.name} size={"sm"} bg={"#EEF2FF"}>
-                                                        <Avatar.Fallback name={user.name} color={"#4F46E5"} />
-                                                        <Avatar.Image src={user.src} />
-                                                    </Avatar.Root>
-                                                ))}
-                                                <Avatar.Root bg={"#EEF2FF"}>
-                                                    <Avatar.Fallback color={"#4F46E5"}>+3</Avatar.Fallback>
-                                                </Avatar.Root>
-                                            </AvatarGroup>
-                                            <Flex gap={4}>
-                                                <Flex align={"center"} gap={1}>
-                                                    <ChatTeardropDotsIcon size={20} weight={"fill"} color={"#94A3B8"} />
-                                                    <Text textStyle={"md"} color={"1E293B"}>12</Text>
-                                                </Flex>
-                                                <Flex align={"center"} gap={1}>
-                                                    <CheckCircleIcon size={20} weight={"fill"} color={"#94A3B8"} />
-                                                    <Text textStyle={"md"} fontWeight={"medium"} color={"1E293B"}>58</Text>
-                                                </Flex>
-                                            </Flex>
-                                        </Flex>
-                                    </Flex>
-                                </Flex>
+                                <Task></Task>
+                                <Task></Task>
+                                <Task></Task>
+                                <Task></Task>
                             </Flex>
-                            <Flex w={"full"}>prog</Flex>
-                            <Flex w={"full"}>compl</Flex>
-                        </Flex>
+
+                            <Flex alignSelf={"flex-start"} w={"full"} p={4} bg={"#F8FAFC"} borderRadius={"4xl"} borderColor={"#E2E8F0"} borderWidth={"1px"} direction={"column"} gap={4}>
+                                <Flex gap={4}>
+                                    <Flex w={"full"} align={"center"} gap={2}>
+                                        <Status.Root>
+                                            <Status.Indicator bg={"#4F46E5"} />
+                                        </Status.Root>
+                                        <Text textStyle={"lg"} fontWeight={"bold"} color={"#1E293B"}>To Do <span style={{ color: '#94A3B8' }}>(8)</span></Text>
+
+                                    </Flex>
+                                    <Button variant="outline" rounded={"full"} w={0}>
+                                        <PlusIcon />
+                                    </Button>
+                                </Flex>
+                                <Task></Task>
+                                <Task></Task>
+                            </Flex>
+
+                            <Flex alignSelf={"flex-start"} w={"full"} p={4} bg={"#F8FAFC"} borderRadius={"4xl"} borderColor={"#E2E8F0"} borderWidth={"1px"} direction={"column"} gap={4}>
+                                <Flex gap={4}>
+                                    <Flex w={"full"} align={"center"} gap={2}>
+                                        <Status.Root>
+                                            <Status.Indicator bg={"#4F46E5"} />
+                                        </Status.Root>
+                                        <Text textStyle={"lg"} fontWeight={"bold"} color={"#1E293B"}>To Do <span style={{ color: '#94A3B8' }}>(8)</span></Text>
+
+                                    </Flex>
+                                    <Button variant="outline" rounded={"full"} w={0}>
+                                        <PlusIcon />
+                                    </Button>
+                                </Flex>
+                                <Task></Task>
+                                <Task></Task>
+                                <Task></Task>
+                            </Flex>
+                        </SimpleGrid>
                     </Tabs.Content>
                     <Tabs.Content value="list">
                         List View
